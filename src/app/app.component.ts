58a1c4cc38745e92f8729980208a4e2d9a63ff80
gauravmishra <gauravmishra590@gmail.com>
2019-04-17 14:52:44 +0530

    again2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,17 @@ export class AppComponent {
 
     url: string;
     result:any
-
+onSelectFile(event) { // called each time file input changes
     
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url = event.target.result;
+      }
+    }
+}
 
 }
