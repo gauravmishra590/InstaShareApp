d9f35af9f5b6e2afc9da53a29a0df9b0d3df8dc7
gauravmishra <gauravmishra590@gmail.com>
2019-04-17 13:51:17 +0530

    again1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+   num1 = 0;
+   num2 = 0;
+   avg : any
+
+   increase( event : Event)
+   {
+     ++this.num1;
+     
+   }
+  
+   increase1(event : Event)
+   {
+    ++this.num2;
+     
+   }
+    Average(event : Event)
+    {
+     this.avg =  (this.num1+this.num2)/2;
+    }
+
+    url: string;
+    result:any
+
+    
+
 }
