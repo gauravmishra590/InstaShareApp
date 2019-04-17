ced76793d9b3ed2d7ad0028007048445d4cc1649
gauravmishra <gauravmishra590@gmail.com>
2019-04-17 21:08:55 +0530

    hello2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
 
@@ -11,29 +13,44 @@ export class AppComponent {
    num2 =0;
    avg : any
 
-   increase( event )
+   increase( event: Event )
    {
      ++this.num1;
      
    }
   
-   increase1(event )
+   increase1(event : Event )
    {
     ++this.num2;
      
    }
-    Average(event )
+    Average(event : Event )
     {
      this.avg =  (this.num1+this.num2)/2;
     }
 
-     reset(even )
+     reset(even : Event )
      {
         this.num1= 0;
         this.num2 = 0;
         this.avg = 0;
 
      }
+      
+     url: string;
+     result: any;
+    
+onSelectFile(event) { 
+    
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
 
+      reader.readAsDataURL(event.target.files[0]); 
+
+      reader.onload = (event) => { 
+        this.url = event.target.result;
+      }
+    }
+} 
      
     }
